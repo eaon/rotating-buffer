85c5083a13679c7a4cc10478e1dab46442aa12dd
eaon <eaon@posteo.net>
2021-06-14 18:31:41 -0400

    Minimise and also don't pretend to be standard APIs when it's inappropriate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+#![no_std]
+
+#[derive(Clone, Copy)]
 pub struct RotatingBuffer<T: Default + Copy, const S: usize> {
     inner_length: usize,
-    inner: [T; S],
-    rotated_length: usize,
+    pub inner: [T; S],
 }
 
 impl<T: Default + Copy, const S: usize> RotatingBuffer<T, S> {
@@ -9,7 +11,6 @@ impl<T: Default + Copy, const S: usize> RotatingBuffer<T, S> {
         RotatingBuffer {
             inner: [T::default(); S],
             inner_length: 0,
-            rotated_length: 0,
         }
     }
 
@@ -17,8 +18,8 @@ impl<T: Default + Copy, const S: usize> RotatingBuffer<T, S> {
         &self.inner[..self.inner_length]
     }
 
-    pub fn as_mut_slice(&mut self) -> &mut [T] {
-        &mut self.inner[self.rotated_length..]
+    pub fn append_only_mut_slice(&mut self) -> &mut [T] {
+        &mut self.inner[self.inner_length..]
     }
 
     pub fn is_empty(&self) -> bool {
@@ -29,27 +30,27 @@ impl<T: Default + Copy, const S: usize> RotatingBuffer<T, S> {
         self.inner_length
     }
 
-    pub fn set_len(&mut self, length: usize) {
-        if length > S {
-            panic!(
-                "Input length larger than buffer capacity: {} > {}",
-                length, S
-            );
-        }
-
-        self.inner_length = self.rotated_length + length;
-        self.rotated_length = 0;
+    pub fn capacity(&self) -> usize {
+        S
     }
 
-    pub fn rotate_starting_at(&mut self, index: usize) {
-        if index > self.inner_length {
-            panic!("Invalid index: {} > {}", index, self.inner_length);
-        }
+    pub fn resize(&mut self, new_len: usize) {
+        assert!(new_len <= S);
+        self.inner_length = new_len;
+    }
 
-        let rotation_length = self.inner_length - index;
-        self.inner[..self.inner_length].rotate_right(rotation_length);
-        self.rotated_length = rotation_length;
-        self.inner_length = rotation_length;
+    pub fn add_len(&mut self, new_len: usize) -> usize {
+        self.resize(self.inner_length + new_len);
+        self.inner_length
+    }
+
+    pub fn rotate_right(&mut self, k: usize) {
+        self.inner[..self.inner_length].rotate_right(k);
+        self.inner_length = k;
+    }
+
+    pub fn rotate_right_at(&mut self, index: usize) {
+        self.rotate_right(self.inner_length - index);
     }
 }
 
@@ -61,52 +62,42 @@ impl<T: Default + Copy, const S: usize> Default for RotatingBuffer<T, S> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::RotatingBuffer;
 
     #[test]
     fn walk_through() {
         let mut buf = RotatingBuffer::<u8, 32>::new();
-        println!("Original length: 0");
+        // Original length: 0
         assert_eq!(0, buf.len());
-        println!("Writable buffer size, flexible, but right now: 32");
-        assert_eq!(32, buf.as_mut_slice().len());
+        // Writable buffer size is flexible, but right now: 32
+        assert_eq!(32, buf.append_only_mut_slice().len());
         // Let's write! More or less compatible with the way UnixStream::try_read works
-        buf.as_mut_slice().copy_from_slice(&[
+        buf.append_only_mut_slice().copy_from_slice(&[
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0,
         ]);
         // Amount of bytes read as returned by UnixStream::try_read
-        buf.set_len(22);
-        println!("Buffer contents: {:?}", buf.as_slice());
-        // Post read, we overflow at index 17
-        buf.rotate_starting_at(17);
-        // New length
+        buf.add_len(22);
+        // Post read, we rotate right at index 17
+        buf.rotate_right_at(17);
+        // New length is now 5
         assert_eq!(5, buf.len());
-        println!("Buffer contents: {:?}", buf.as_slice());
         // Write a full message to the buffer! 27 new spicey bytes
-        buf.as_mut_slice().copy_from_slice(&[
+        buf.append_only_mut_slice().copy_from_slice(&[
             22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
             44, 45, 46, 47, 48,
         ]);
-        buf.set_len(24);
-        println!(
-            "Wrote 24 new bytes to buffer\nNew buffer contents: {:?}",
-            buf.as_slice()
-        );
+        buf.add_len(24);
         assert_eq!(29, buf.len());
-        // Overflow again at 21
-        buf.rotate_starting_at(21);
+        // Rotating again at 21
+        buf.rotate_right_at(21);
         assert_eq!(8, buf.len());
         // More new data
-        buf.as_mut_slice().copy_from_slice(&[
+        buf.append_only_mut_slice().copy_from_slice(&[
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 0, 0, 0, 0, 0, 0, 0,
         ]);
-        // And the data length
-        buf.set_len(16);
-        println!(
-            "Wrote 16 new bytes to buffer\nNew buffer contents: {:?}",
-            buf.as_slice()
-        );
+        // And the new data length
+        buf.add_len(16);
         assert_eq!(24, buf.len());
     }
 }
